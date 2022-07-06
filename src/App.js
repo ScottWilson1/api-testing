1c7df8e3097e5cc93c430ce0223072519e5b85ce
ScottWilson1 <scott.wilson@detroitlabs.com>
2022-07-06 11:56:48 -0400

    implemented Contentful api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [answerTypes, setAnswerTypes] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      const data = await fetch(`https://cdn.contentful.com/spaces/${process.env.REACT_APP_CONTENTFUL_SPACE_ID}/environments/master/entries/${process.env.REACT_APP_CONTENTFUL_ENTRY_ID}?access_token=${process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN}`);
+      const json = await data.json();
+      console.log(json);
+      const questionAndAnswers = json.fields.question1;
+      setQuestion(questionAndAnswers.shift());
+      setAnswerTypes(questionAndAnswers);
+    }
+      fetchQuestion().catch(console.error);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{question}</p>
+      {console.log(answerTypes)}
+      <div>
+        {answerTypes.map((a, key) => (
+          <button key={key}>{a}</button>
+        ))}
+      </div>
     </div>
   );
 }
